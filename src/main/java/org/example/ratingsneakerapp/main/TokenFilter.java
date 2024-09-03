1da4109e323d86e7ab34e77bd138917fd04c7acb
Администратор <asdasad@gmail.com>
2024-09-03 18:37:26 +0300

    Commit all project 9

--- src/main/java/org/example/ratingsneakerapp/main/TokenFilter.java
+++ src/main/java/org/example/ratingsneakerapp/main/TokenFilter.java
@@ -1,10 +1,10 @@
 package org.example.ratingsneakerapp.main;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.ratingsneakerapp.JwtCore;
@@ -12,19 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 
 @Component
 public class TokenFilter  extends OncePerRequestFilter {
@@ -44,27 +39,36 @@ public class TokenFilter  extends OncePerRequestFilter {
          }
         String role = null;
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        try {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
         String authorizationHeader = httpRequest.getHeader("Authorization");
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-            Claims claims = Jwts.parser()
-                        .setSigningKey(secret)
-                        .parseClaimsJws(jwt)
-                        .getBody();
-            role = claims.getSubject();
-            if (role != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
-
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        null, null, Collections.singletonList(authority));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                jwt = authorizationHeader.substring(7);
             }
+            if (jwt != null) {
+                    try {
+                    Claims claims = Jwts.parser()
+                            .setSigningKey(secret)
+                            .parseClaimsJws(jwt)
+                            .getBody();
+                    role = claims.getSubject();
+                    if (role != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
 
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            null, null, Collections.singletonList(authority));
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    }
+                    } catch (ExpiredJwtException ignored) {
+                    }
+            }
+        }catch (Exception e) {
 
         }
+
+
 
         filterChain.doFilter(request, response);
         }
